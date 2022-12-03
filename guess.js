3e17204705c2fb88c3dec7e2bbadf2abbeabf64a
Rog666 <23439569+Rog666@users.noreply.github.com>
2022-12-03 10:37:44 +0000

    Fixed index.html to link to js file correcdtly

--- guess.js
+++ guess.js
@@ -1,12 +1,12 @@
 var tries =0;
 var ans=Math.floor(Math.random()*100);
-var guess, name;
-name = prompt("Enter your name:");
+var guess;
+var name = prompt("Enter your name:");
 for(guess=1;guess!=ans;tries++) {
-guess= prompt("Guess the number between 1 and 100. ") ;
-if (guess <ans) {alert(" too low") ;} 
-if (guess >ans)  {alert ("too high") ;} 
-if (guess == 101) {alert ("The secret number is " + ans)};
+    guess= prompt("Guess the number between 1 and 100. ") ;
+    if (guess <ans) {alert(" too low") ;} 
+    if (guess >ans)  {alert ("too high") ;} 
+    if (guess == 101) {alert ("The secret number is " + ans)};
 } 
 alert ("Well done " + name + " you got it in " + tries + " tries.") ;
 
